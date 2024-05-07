181e60e8d4bf81ae54040c5fd7cf6e5e809ff21a
ssuvamm <ssuvamm@gmail.com>
2024-05-07 20:57:53 +0530

    bg, scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,28 @@ function App() {
         <Route path="*" element={<Error />}></Route>
       </Routes>
       <Footer />
+
       <ScrollToTop
         smooth
         top={720}
-        viewBox="0 0 384 512"
-        svgPath="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
-        height="40"
-        width="40"
-        className="flex animate-bounce rounded-lg bg-indigo-600 hover:animate-none hover:bg-indigo-700"
+        component={
+          <svg
+            className=""
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m16 17-4-4-4 4m8-6-4-4-4 4"
+            />
+          </svg>
+        }
+        className=" animate-bounce rounded-lg bg-white text-[#1ab4d8] hover:animate-none hover:bg-gray-200"
       />
     </>
   );
